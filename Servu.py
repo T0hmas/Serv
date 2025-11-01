7c1e19763a91a24d0a77c4f02ba9867654db9a6c
Tomas Chen <yliharsilatomas@gmail.com>
2025-11-01 11:19:03 +0200

    fix

--- Servu.py
+++ Servu.py
@@ -2,8 +2,8 @@ from wsgiref.simple_server import make_server
 
 def app(environ, respond):
 	respond('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
-	yield "Hello world :) !".encode('utf-8')
-
+	yield "Moi Twin :3 ".encode('utf-8')
+   
 if __name__ == '__main__':
 	with make_server("localhost", 8000, app) as server: 
             server.serve_forever()   
